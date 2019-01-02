e161cc852a5e476253530a1c8beb969974ae74de
jessepackwood <packwoodjesse@gmail.com>
2019-01-01 22:58:41 -0700

    suitcase created

---
+++ src/components/Suitcase/Suitcase.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Suitcase.css';
+
+const Suitcase = (props) => {
+  return (
+    <div className='suitcase'>
+      Suitcase
+    </div>
+  )
+}
+
+export default Suitcase;
