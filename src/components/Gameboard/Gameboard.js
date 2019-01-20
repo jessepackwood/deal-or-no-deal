24f5d4120085450aa5100c3a77769df0c58caace
jessepackwood <packwoodjesse@gmail.com>
2019-01-20 11:14:47 -0700

    sort function for shuffling and assigning money values

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Suitcase from '../../components/Suitcase/Suitcase';
+import Suitcase from '../Suitcase/Suitcase';
+import './Gameboard.css';
+
+const MoneyValues = [.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
 
 export default class Gameboard extends Component {
   constructor() {
     super();
-
-    let caseValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-    let moneyValues = [.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
+    
+    let suitcases = this.buildSuitcases()
 
     this.state = {
-      suitcases: [...caseValues],
-      moneySlots: [...moneyValues],
+      suitcases: suitcases,
+      // moneySlots here in state will be the values you list next to the board. Don't shuffle these
+      moneySlots: MoneyValues.slice(),
     }
   }
 
@@ -21,20 +24,79 @@ export default class Gameboard extends Component {
     })
   }*/
 
-  render() {
+  componentDidMount() {
+  }
 
+  updateSuitcaseNumbers(remainingCases) {
+    this.setState({suitcases: remainingCases})
+  }
+
+  updateSuitcaseMoney(remainingMoney) {
+    this.setState({moneySlots: remainingMoney})
+  }
+
+  buildSuitcases = () => {
+    let builtSuitcases = [];
+    // shuffle money values and return new shuffled array
+    let shuffledMoney = this.shuffleMoney();
+
+    // for each value in the array returned push a suitcase object onto built cases
+
+    for(let i = 0; i < shuffledMoney.length; i++) {
+      builtSuitcases.push({
+        number: i + 1,
+        money: shuffledMoney[i]
+      })
+    }
+    
+    return builtSuitcases
+  }
+
+
+  shuffleMoney() {
+    let valueArray = MoneyValues.slice(); //Make a copy to preserve origin of truth
+    let currentIndex = valueArray.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = valueArray[currentIndex];
+      valueArray[currentIndex] = valueArray[randomIndex];
+      valueArray[randomIndex] = temporaryValue;
+    }
+
+    console.log(valueArray);
+
+    return valueArray
+  }
+  
+
+  render() {
     const mappedSuitcases = this.state.suitcases.map( (suitcase, index) => {
-      return <Suitcase
-                value={suitcase}
-                key={`Case: ${index + 1}`}
-                money={0}
-              />
+    console.log(suitcase)
+    return <Suitcase
+              number={suitcase.number}
+              key={suitcase.number.toString()}
+              money={suitcase.money}
+            />
     })
 
     return (
-      <div>
+      <div className='game-board'>
         {mappedSuitcases}
       </div>
     )
   }
+}
+
+Gameboard.propTypes = {
+  value: PropTypes.number,
+  money: PropTypes.number,
 }
