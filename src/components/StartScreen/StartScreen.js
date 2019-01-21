752a478c0d64da49b2f3bed11f6156a6aa0fa533
jessepackwood <packwoodjesse@gmail.com>
2019-01-20 22:09:08 -0700

    create start screen

---
+++ src/components/StartScreen/StartScreen.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+export default class StartScreen extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      begin: true
+    }
+  }
+
+  render() {
+    return(
+      <div className='start-screen'>
+      </div>
+    )
+  }
+}
