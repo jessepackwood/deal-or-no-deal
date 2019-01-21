502567c771c337f660b15570215a7547d6923be4
jessepackwood <packwoodjesse@gmail.com>
2019-01-21 02:33:49 -0700

    pass down className as a prop

--- src/components/MoneySlot/MoneySlot.js
+++ src/components/MoneySlot/MoneySlot.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Suitcase from '../Suitcase/Suitcase';
 import NumberFormat from 'react-number-format-presuffix';
+import './MoneySlot.css';
 
 const MoneySlot = (props) => {
   return (
-    <div
-      className = 'money-slot'
-    >
+    <div className={props.className}>
       <NumberFormat value={props.money} displayType={'text'} thousandSeparator={true} prefix={'$'} />
     </div>
   )
