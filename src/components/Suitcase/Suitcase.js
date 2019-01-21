33673ce12dca68773f3739a19b4bea1c15ca67ed
jessepackwood <packwoodjesse@gmail.com>
2019-01-20 22:10:15 -0700

    add ability to remove suitcase

--- src/components/Suitcase/Suitcase.js
+++ src/components/Suitcase/Suitcase.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Suitcase.css';
+import NumberFormat from 'react-number-format-presuffix';
 
 const Suitcase = (props) => {
   return (
-    <div className='suitcase'>
+    <div 
+      className = 'suitcase'
+      onClick = { () => props.removeSuitcase(props.number) }
+    >
       Number = {props.number}
       <div>
-        Money = {props.money}
+        Money = <NumberFormat value={props.money} displayType={'text'} thousandSeparator={true} prefix={'$'} />
       </div>
     </div>
   )
