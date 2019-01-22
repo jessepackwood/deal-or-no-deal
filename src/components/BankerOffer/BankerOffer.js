4497287102f99e8d956c7e2b2f5d0b494c6dfe35
jessepackwood <packwoodjesse@gmail.com>
2019-01-21 19:17:06 -0700

    create bank offer component

--- src/components/BankerOffer/BankerOffer.js
+++ src/components/BankerOffer/BankerOffer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './BankerOffer.css';
 
-const BankerOffer = () => {
+const BankerOffer = (props) => {
   return(
     <div className='banker-offer'>
+      
       <button label='Deal' />
       <button label='No Deal' />
     </div>
