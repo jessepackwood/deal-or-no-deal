e6bc0304adfeb509430fbd7917a57319c4519bb4
jessepackwood <packwoodjesse@gmail.com>
2019-01-22 18:48:21 -0700

    create test suite for moneyslot

---
+++ src/components/MoneySlot/MoneySlot.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import MoneySlot from './MoneySlot';
+
+describe('MoneySlot', () => {
+  it('should exist', () => {
+    expect(MoneySlot).toBeDefined();
+  })
+
+})
